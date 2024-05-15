811329688f4667317b0dd4aa5b4fe8488bdb2e2b
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-05-15 15:39:17 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/vanlife@e753b7b73140192558695324bfcb8b8a523949eb 🚀

--- assets/Income-CDRJ5eId.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BlE7lBJp.js";function o(){return e.jsx("div",{children:"Income page"})}export{o as default};
--- assets/Reviews-Cpi41wTs.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BlE7lBJp.js";function r(){return e.jsx("div",{children:"Reviews page"})}export{r as default};
